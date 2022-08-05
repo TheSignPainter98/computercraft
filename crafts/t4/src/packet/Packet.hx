43d0463fdd7873d8594b9551e3326045a0c6e1ee
Ed Jones <edjones98@gmail.com>
2022-08-05 22:36:47 +0100

    Added server rednet stubs

--- crafts/t4/src/packet/Packet.hx
+++ crafts/t4/src/packet/Packet.hx
@@ -2,6 +2,13 @@ package packet;
 
 import model.SignalState;
 
+enum Packet {
+	SignalToServer(pkt: SignalToServerPacket);
+	YardToServer(pkt: YardToServerPacket);
+	ServerToSignal(pkt: ServerToSignalPacket);
+	ServerToYard(pkt: ServerToYardPacket);
+}
+
 enum SignalToServerPacket {
 	Register(connected: Array<Int>);
 	State(id: Int, signal: SignalState);
--- crafts/t4/src/server/Server.hx
+++ crafts/t4/src/server/Server.hx
@@ -1,21 +1,91 @@
 package server;
 
+import argparse.ArgAccessor;
+import argparse.ArgParser;
+import argparse.ProgSpec;
 import cc.OS;
+import cc.Rednet;
 import config.Config;
 import events.Event;
 import events.EventEmitter;
 import events.CustomEvent.EVENT_SAVE_INVALIDATED;
+import events.OSEvent.EVENT_REDNET_MESSAGE;
 import events.OSEvent.EVENT_TERMINATE;
+import events.OSEvent.RednetMessageEvent;
+import packet.Packet;
 
 class Server {
+	public static inline final SERVER_PROTOCOL = "t4-server";
+
+	private static final NETWORK_NAME = new ArgAccessor<String>();
+
+	private static var cliSpec: ProgSpec = {
+		name: "t4-server",
+		shortDesc: "t4 central server",
+		desc: "Single source of truth for t4's belief about the current status of its network",
+		author: "The authors of t4",
+		date: "2022",
+		license: [
+			"This program is free software: you can redistribute it and/or modify",
+			"it under the terms of the GNU General Public License as published by",
+			"the Free Software Foundation, either version 3 of the License, or",
+			"(at your option) any later version.",
+			"",
+			"This program is distributed in the hope that it will be useful,",
+			"but WITHOUT ANY WARRANTY; without even the implied warranty of",
+			"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
+			"GNU General Public License for more details.",
+			"",
+			"You should have received a copy of the GNU General Public License",
+			"along with this program.  If not, see <https://www.gnu.org/licenses/>.",
+		],
+		positionals: [
+		{
+			dest: NETWORK_NAME,
+			desc: "The name of the network which this server controlls (currently unused)",
+			type: String(null),
+			dflt: "universe",
+			trigger: {
+				metavar: "network",
+			},
+		}
+		],
+		flags: [],
+	}
+
 	public static function main(args: Array<String>, settings: Config) {
 		trace("I am a server.");
+
+		init(args, settings);
 
 		var emitter = new EventEmitter();
 
 		emitter.addEventListener(EVENT_SAVE_INVALIDATED, (_) -> settings.save());
 		emitter.addEventListener(EVENT_TERMINATE, (_) -> emitter.stopListening());
+		emitter.addEventListener(EVENT_REDNET_MESSAGE, processRednetEvent);
 
 		emitter.listen();
+	}
+
+	private static function init(argv: Array<String>, settings: Config): Bool {
+		final parser = new ArgParser(cliSpec);
+		final args = parser.parse(argv);
+		if (args == null)
+			return false;
+
+		Rednet.host(SERVER_PROTOCOL, args[NETWORK_NAME]);
+
+		return true;
+	}
+
+	private static function processRednetEvent(e: RednetMessageEvent) {
+		if (e.protocol != SERVER_PROTOCOL)
+			return;
+
+		processPacket(e.message);
+	}
+
+	private static function processPacket(pkt: Packet) {
+		trace('Got packet $pkt');
 	}
 }
