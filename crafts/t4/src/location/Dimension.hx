25c11d4f7191c05e5c5372f700d866abd44ce82c
Ed Jones <edjones98@gmail.com>
2022-08-12 00:21:46 +0100

    Improved point abstractions

---
+++ crafts/t4/src/location/Dimension.hx
@@ -0,0 +1,7 @@
+package location;
+
+enum Dimension {
+	Overworld;
+	Nether;
+	// End; // It is not currently possible to get trains into the End.
+}
--- crafts/t4/src/location/Point.hx
+++ crafts/t4/src/location/Point.hx
@@ -1,41 +1,65 @@
 package location;
 
 import cc.GPS.GPSLocation;
-import haxe.ds.Vector;
-import haxe.ValueException;
+import haxe.ds.Option;
 
 @:structInit
 class PointImpl {
 	public final x: Int;
 	public final y: Int;
 	public final z: Int;
+	public final dimension: Dimension;
 }
 
+@:forward
 abstract Point(PointImpl) from PointImpl to PointImpl {
 	@:from
 	public static function fromGPSLocation(raw: GPSLocation): Point {
-		return Point {
-			x: raw.x,
-			y: raw.y,
-			z: raw.z,
+		return {
+			x: Std.int(raw.x),
+			y: Std.int(raw.y),
+			z: Std.int(raw.z),
+			dimension: Overworld, // TODO: Get the actual dimension
 		}
 	}
 
 	@:op(A + B)
-	public function add(other: Point): Point {
-		return {
+	public function add(other: Point): Option<Point> {
+		if (this.dimension != other.dimension)
+			return None;
+		return Some({
 			x: this.x + other.x,
 			y: this.y + other.y,
 			z: this.z + other.z,
-		}
+			dimension: this.dimension,
+		});
 	}
 
 	@:op(A * B)
-	public function mult(lambda: Int): Point {
+	public function mult(lambda: Float): Point {
 		return {
-			x: this.x * lambda,
-			y: this.y * lambda,
-			z: this.z * lambda,
+			x: Std.int(this.x * lambda),
+			y: Std.int(this.y * lambda),
+			z: Std.int(this.z * lambda),
+			dimension: this.dimension,
+		}
+	}
+
+	public static inline function origin(dimension: Dimension): Point {
+		return {
+			x: 0,
+			y: 0,
+			z: 0,
+			dimension: dimension,
+		}
+	}
+
+	public static inline function worldOrigin(): Point {
+		return {
+			x: 0,
+			y: 0,
+			z: 64,
+			dimension: Overworld,
 		}
 	}
 }
