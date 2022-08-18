40f122b4371c5f914a419694b8acf19f25cdf0fe
Ed Jones <edjones98@gmail.com>
2022-08-18 22:33:46 +0100

    Stations push their status to the server

--- crafts/t4/src/server/Server.hx
+++ crafts/t4/src/server/Server.hx
@@ -13,6 +13,7 @@ import events.CustomEvent.EVENT_SAVE_INVALIDATED;
 import events.OSEvent.EVENT_REDNET_MESSAGE;
 import events.OSEvent.EVENT_TERMINATE;
 import events.OSEvent.RednetMessageEvent;
+import events.OSEvent.EVENT_TIMER;
 import extype.Result;
 import extype.Tuple.Tuple2;
 import extype.Unit;
@@ -20,18 +21,52 @@ import extype.Unit._;
 import logger.Logger;
 import rednetmgr.Header;
 import rednetmgr.RednetManager;
+import rednetmgr.MessageTag;
 import station.Station;
+import station.Station.PULSE as STATION_PULSE;
+import station.Station.STATION_PROTOCOL;
 import station.StationDeclaration;
 import station.StationStatus;
 import server.model.Station as StationModel;
 
+// import periphs.TrainNetworkObserver.TrainID;
+
+enum TrainMovement {
+	AwaitingSignal;
+	AtStationPlatform;
+	Moving(speed: Float);
+}
+
+@:structInit
+class TrainStatus {
+	public final name: String;
+	public final id: String; // TODO(kcza): TrainID
+	public final callingAtPlatforms: Array<String>; // TODO(kcza): StopID
+	public final callingAt: Array<Int>; // TODO(kcza): refactor station ID to its own type?
+	public final movement: TrainMovement;
+}
+
+@:structInit
+private class State {
+	public var emissionTimer: Int;
+	public var stationPulseTimer: Int;
+	public var stationStatuses: Array<StationStatus>;
+	public var aliveStations: Array<Int>;
+	public var trainStatuses: Map<String, TrainStatus>; // TODO(kcza): TrainID
+}
+
 class Server {
 	public static inline final SERVER_PROTOCOL = "t4-server";
+	public static inline final PULSE_RESPONSE: MessageTag<Unit> = "t4-server:pulse-response";
+	public static inline final STATION_STATUS_DECLARE: MessageTag<StationStatus> = "t4-server-station-status-declare";
+
 	private static inline final KNOWN_STATIONS: Accessor<Map<Int, StationModel>> = "known-stations";
+	private static final EMISSION_PERIOD = new ArgAccessor<Int>();
+	private static final PULSE_PERIOD = new ArgAccessor<Int>();
 
-	private static var stationStatuses: Array<StationStatus> = [];
+	private static final stationStatuses: Array<StationStatus> = [];
 
-	private static var cliSpec: ProgSpec = {
+	private static final cliSpec: ProgSpec = {
 		name: "t4-server",
 		shortDesc: "t4 central server",
 		desc: "Single source of truth for t4's belief about the current status of its network",
@@ -51,6 +86,28 @@ class Server {
 			"You should have received a copy of the GNU General Public License",
 			"along with this program.  If not, see <https://www.gnu.org/licenses/>.",
 		],
+		flags: [
+			{
+				dest: EMISSION_PERIOD,
+				desc: "The number of seconds between the server passing its knowledge of the system status to displays",
+				type: Int(null),
+				dflt: 10,
+				trigger: {
+					short: "-e",
+					long: "--emit-every",
+				},
+			},
+			{
+				dest: PULSE_PERIOD,
+				desc: "The number of seconds between the server checking that its known stations are still alive",
+				type: Int(null),
+				dflt: 100,
+				trigger: {
+					short: "-p",
+					long: "--check-pulse-every",
+				},
+			},
+		],
 	}
 
 	public static function main(t4Args: Args, settings: Config): Result<Unit, String> {
@@ -63,6 +120,14 @@ class Server {
 		var emitter = new EventEmitter();
 		var rednet = new RednetManager();
 
+		final state: State = {
+			emissionTimer: OS.startTimer(args[EMISSION_PERIOD]),
+			stationPulseTimer: OS.startTimer(args[PULSE_PERIOD]),
+			stationStatuses: [],
+			aliveStations: [],
+			trainStatuses: [],
+		};
+
 		switch (rednet.open(t4Args[Main.MODEM], t4Args[Main.DEBUG_MODE])) {
 			case Failure(err):
 				return Failure(err);
@@ -70,49 +135,30 @@ class Server {
 		}
 		rednet.host(SERVER_PROTOCOL, t4Args[Main.NETWORK]);
 
-		rednet.addResponse(SERVER_PROTOCOL, Station.RESOLVE_ID, (hdr, msg) -> resolveID(rednet, settings, hdr, msg));
+		rednet.addResponse(SERVER_PROTOCOL, PULSE_RESPONSE, (hdr, _) -> state.aliveStations.push(hdr.src));
 
 		emitter.addEventListener(EVENT_SAVE_INVALIDATED, (_) -> settings.save());
 		emitter.addEventListener(EVENT_REDNET_MESSAGE, rednet.onRednetMessageEvent);
+		emitter.addEventListener(EVENT_TIMER, (e) -> {
+			final timer = e.get_id();
+			if (timer == state.emissionTimer) {
+				Logger.log('Broadcasting system status');
+				// broadcastSystemStatus(aggregateSystemStatus(state.trainStatuses, state.stationStatuses));
+				state.emissionTimer = OS.startTimer(args[EMISSION_PERIOD]);
+			} else if (timer == state.stationPulseTimer) {
+				Logger.log('Checking station pulse');
+				// state.stationStatuses = purge(state.stationStatuses);
+				rednet.broadcast(STATION_PROTOCOL, STATION_PULSE, _);
+				state.stationPulseTimer = OS.startTimer(args[PULSE_PERIOD]);
+			} else {
+				Logger.log('Unknown timer: ${e.get_id()}?');
+			}
+		});
 
 		emitter.listen();
 
 		rednet.close();
 
 		return Success(_);
-	}
-
-	private static function resolveID(rednet: RednetManager, settings: Config, hdr: Header, hint: StationDeclaration) {
-		var knownStations = settings.setDefault(KNOWN_STATIONS, () -> new Map());
-
-		final id = switch (hint.idSuggestion) {
-			case Some(suggestedId):
-				final model = knownStations[suggestedId];
-				if (model == null || model.hostId == hdr.src)
-					suggestedId;
-				else
-					freshStationId(knownStations);
-			case None:
-				freshStationId(knownStations);
-		}
-
-		final station: Dynamic = {
-			id: id,
-			hostId: hdr.src,
-			name: hint.name,
-		}
-		knownStations[id] = station;
-
-		rednet.sendDirect(hdr.protocol, hdr.src, Station.RESOLVE_ID_RESPONSE, id);
-
-		settings.invalidate();
-	}
-
-	private static function freshStationId(known: Map<Int, Dynamic>): Int {
-		while (true) {
-			final rand = Std.int(Math.random());
-			if (known[rand] == null)
-				return rand;
-		}
 	}
 }
--- crafts/t4/src/station/Station.hx
+++ crafts/t4/src/station/Station.hx
@@ -5,19 +5,22 @@ import argparse.ArgParser;
 import argparse.Args;
 import argparse.ProgSpec;
 import cc.GPS;
-import cc.Rednet;
+import cc.OS;
 import cc.Peripheral;
 import config.Accessor;
 import config.Config;
 import events.EventEmitter;
 import events.CustomEvent.EVENT_SAVE_INVALIDATED;
 import events.OSEvent.EVENT_REDNET_MESSAGE;
+import events.OSEvent.EVENT_TIMER;
 import haxe.ds.Option;
 import haxe.Exception;
 import lua.Table;
 import server.Server;
+import server.Server.PULSE_RESPONSE;
+import server.Server.SERVER_PROTOCOL;
+import server.Server.STATION_STATUS_DECLARE;
 import rednetmgr.MessageTag;
-import rednetmgr.Packet.StationToServerPacket;
 import rednetmgr.RednetManager;
 import location.Point;
 import logger.Logger;
@@ -27,15 +30,12 @@ import extype.Unit._;
 
 class Station {
 	public static inline final STATION_PROTOCOL = "t4-station";
-	public static inline final STATUS_REQUEST: MessageTag<Void> = "t4-station-request-status";
-	public static inline final STATUS_DECLARE: MessageTag<StationStatus> = "t4-station-declare-status";
-	public static inline final RESOLVE_ID: MessageTag<StationDeclaration> = "t4-station-resolve-id";
-	public static inline final RESOLVE_ID_RESPONSE: MessageTag<Int> = "t4-station-id-resolve-response";
 	public static inline final PULSE: MessageTag<Unit> = "t4-station-pulse";
 
-	private static final NAME = new ArgAccessor<String>();
+	private static final STATUS_EMISSION_PERIOD = new ArgAccessor<Int>();
+	private static inline final STATUS_EMISSION_PERIOD_DEFAULT = 10;
 
-	private static inline final ID: Accessor<Int> = "id";
+	private static final NAME = new ArgAccessor<String>();
 
 	private static var cliSpec: ProgSpec = {
 		name: "t4 station",
@@ -53,10 +53,22 @@ class Station {
 				},
 			}
 		],
+		flags: [
+			{
+				dest: STATUS_EMISSION_PERIOD,
+				desc: "The number of seconds between the successive messages to the central server",
+				type: Int(null),
+				dflt: STATUS_EMISSION_PERIOD_DEFAULT,
+				trigger: {
+					short: "-e",
+					long: "--emit-every",
+				},
+			},
+		],
 	}
 
-	// private static var serverID: Int;
-	private static var id: Int;
+	private static final id: Int = OS.getComputerID();
+	private static final stationHostname: String = 'station-#$id';
 	private static var name: Null<String>;
 
 	public static function main(t4Args: Args, settings: Config): Result<Unit, String> {
@@ -75,14 +87,24 @@ class Station {
 			default:
 		}
 
-		rednet.addResponse(STATION_PROTOCOL, STATUS_REQUEST, (_, _) -> rednet.send(STATION_PROTOCOL, t4Args[Main.NETWORK], STATUS_DECLARE, status()));
+		rednet.addResponse(STATION_PROTOCOL, PULSE, (hdr, msg) -> rednet.sendDirect(SERVER_PROTOCOL, hdr.src, PULSE_RESPONSE, _));
+
+		rednet.host(STATION_PROTOCOL, stationHostname);
 
 		Logger.log('This station is `$name.\'');
+
+		var emissionTimer = OS.startTimer(args[STATUS_EMISSION_PERIOD]);
 
 		var emitter = new EventEmitter();
 
 		emitter.addEventListener(EVENT_SAVE_INVALIDATED, (_) -> settings.save());
 		emitter.addEventListener(EVENT_REDNET_MESSAGE, rednet.onRednetMessageEvent);
+		emitter.addEventListener(EVENT_TIMER, (e) -> {
+			if (e.get_id() == emissionTimer) {
+				rednet.send(SERVER_PROTOCOL, t4Args[Main.NETWORK], STATION_STATUS_DECLARE, status());
+				emissionTimer = OS.startTimer(args[STATUS_EMISSION_PERIOD]);
+			}
+		});
 
 		emitter.listen();
 
@@ -105,39 +127,10 @@ class Station {
 			loc;
 		}
 
-		id = switch (negotiateID(rednet, settings, name)) {
-			case Success(id): id;
-			case Failure(err): return Failure(err);
-		};
-		Logger.log('Received ID: $id');
-
-		rednet.host(STATION_PROTOCOL, stationProtocolHostname(id));
-
 		return Success(_);
 	}
 
-	public static function negotiateID(rednet: RednetManager, options: Config, name: String): Result<Int, String> {
-		var initialID = options[ID];
-		final hint: StationDeclaration = {
-			name: name,
-			idSuggestion: if (initialID == null) None else Some(initialID),
-		};
-
-		switch (rednet.get(Server.SERVER_PROTOCOL, null, RESOLVE_ID_RESPONSE, RESOLVE_ID, hint)) {
-			case Success(id):
-				options[ID] = id;
-				return Success(id);
-			case Failure(e):
-				return Failure(e);
-		}
-	}
-
-	private static function stationProtocolHostname(id: Int): String {
-		return 'station-#$id';
-	}
-
-	public static function status(): StationStatus {
-		return null;
+	private static function status(): StationStatus {
 		return {
 			id: id,
 			name: name,
@@ -146,15 +139,15 @@ class Station {
 		};
 	}
 
-	public static function updatePlatforms() {}
+	private static function updatePlatforms() {}
 
-	public static function updateBuffers() {}
+	private static function updateBuffers() {}
 
-	public static function aggregateBufferStatus(): Option<StationBufferStatus> {
+	private static function aggregateBufferStatus(): Option<StationBufferStatus> {
 		return None;
 	}
 
-	public static function servicingStatus(): Array<StationPlatformOperation> {
+	private static function servicingStatus(): Array<StationPlatformOperation> {
 		return [];
 	}
 }
